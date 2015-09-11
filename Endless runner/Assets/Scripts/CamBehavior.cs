5708b9db552b2de74f6465a28274740b54275a52
tommtom9 <tommtom9@live.nl>
2015-09-11 11:20:55 +0200

    Added project so far
    I added the project files we have so far so my teammate can work on them
    as wel

---
+++ Endless runner/Assets/Scripts/CamBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamBehavior : MonoBehaviour {
+
+	Transform player;
+	public float horizontalOffset = 0f;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+
+	void Update () {
+		var x = player.position.x;
+
+		transform.position = new Vector3 (x + horizontalOffset, transform.position.y, -10);
+	}
+}
---
+++ Endless runner/Assets/Scripts/SpawnHandeler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnHandeler : MonoBehaviour {
+
+    public GameObject[] stageCollection;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "SpawnTrigger")
+        {
+            var stage = other.gameObject;
+			Transform spawnLocation = stage.transform.parent.Find("Spawnlocation");
+            GameObject obj = Instantiate(stageCollection[Random.Range(0, stageCollection.Length)],
+                spawnLocation.position, 
+                Quaternion.identity) as GameObject;
+
+            obj.name = "SpawnedStage";
+        }
+
+		if (other.gameObject.name == "Spawnlocation")
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+    }
+}
